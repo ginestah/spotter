62726e7bd15321fca75c6732cadf087e6ee99fde
ginestah <68362184+ginestah@users.noreply.github.com>
2021-02-22 09:46:53 -0500

    added styling and moved some styles to other folders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
+import "./App.css";
 
+import { Route } from "react-router-dom";
 
-import { Route } from 'react-router-dom';
-
-import Navbar from './components/Navbar/Navbar';
-import Boulder from './components/Boulder/Boulder';
-import Form from './components/Form/Form';
-import About from './components/About/About';
-import Details from './components/Details/Details';
-import Footer from './components/Footer/Footer';
-import Search from './components/Search/Search'
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { baseURL, config } from './services';
+import Navbar from "./components/Navbar/Navbar";
+import Boulder from "./components/Boulder/Boulder";
+import Form from "./components/Form/Form";
+import About from "./components/About/About";
+import Details from "./components/Details/Details";
+import Footer from "./components/Footer/Footer";
+import Search from "./components/Search/Search";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { baseURL, config } from "./services";
 
 function App() {
   const [boulders, setBoulders] = useState([]);
@@ -29,28 +28,27 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Route exact path ="/">
+      <Route exact path="/">
         <h2>Click on a photo for more details of the climb</h2>
-        </Route>
-      <Route exact path='/'>
-        {boulders.map((boulder => (
+      </Route>
+      <Route exact path="/">
+        {boulders.map((boulder) => (
           <Boulder key={boulder.id} boulder={boulder} />
-        )))}
-        </Route>
+        ))}
+      </Route>
       <Route path="/details/:id">
         <Details setToggleFetch={setToggleFetch} boulders={boulders} />
       </Route>
-      <Route exact path='/new'>
+      <Route exact path="/new">
         <Form setToggleFetch={setToggleFetch} />
       </Route>
-      <Route exact path='/about'>
-        <About/>
+      <Route exact path="/about">
+        <About />
       </Route>
-      <Route path='/search'>
+      <Route path="/search">
         <Search boulders={boulders} />
-        </Route>
+      </Route>
       <Footer />
-     
     </div>
   );
 }
--- src/components/Boulder/Boulder.jsx
+++ src/components/Boulder/Boulder.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./Boulder.css";
 
 function Boulder(props) {
   return (
